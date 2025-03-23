a9b9d60dcc746057c289e3d3042917c10c5eed8c
leeminjxun <th554751@naver.com>
2025-03-23 23:55:45 +0900

    Feat: 공지사항(Notice) 페이징 처리 기능 구현(전체 / 카테고리 조회)

--- src/main/java/org/ayu/doyouknowback/notice/repository/NoticeRepository.java
+++ src/main/java/org/ayu/doyouknowback/notice/repository/NoticeRepository.java
@@ -1,6 +1,10 @@
 package org.ayu.doyouknowback.notice.repository;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
 import org.ayu.doyouknowback.notice.domain.Notice;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,7 +15,13 @@ import java.util.List;
 @Repository
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
+    @NonNull
+    Page<Notice> findAll(@NonNull Pageable pageable);
+
+//    @Query("select n from Notice n where n.noticeCategory = :noticeCategory")
+//    List<Notice> findAllByCategory(@Param("noticeCategory") String noticeCategory);
+
     @Query("select n from Notice n where n.noticeCategory = :noticeCategory")
-    List<Notice> findAllByCategory(@Param("noticeCategory") String noticeCategory);
+    Page<Notice> findAllByCategory(@Param("noticeCategory") String noticeCategory, Pageable pageable);
 
 }
