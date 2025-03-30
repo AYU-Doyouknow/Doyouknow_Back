0d4585e5d341f4e7d3494a521875bd7ce8a9b233
dongyeop00 <ehd_duq94@naver.com>
2025-03-30 14:40:00 +0900

    Feat: Notice 페이지네이션 구현

--- src/main/java/org/ayu/doyouknowback/notice/controller/NoticeController.java
+++ src/main/java/org/ayu/doyouknowback/notice/controller/NoticeController.java
@@ -6,6 +6,9 @@ import org.ayu.doyouknowback.notice.form.NoticeDetailResponseDTO;
 import org.ayu.doyouknowback.notice.form.NoticeRequestDTO;
 import org.ayu.doyouknowback.notice.form.NoticeResponseDTO;
 import org.ayu.doyouknowback.notice.service.NoticeService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,30 +22,55 @@ public class NoticeController {
 
     private final NoticeService noticeService;
 
-    @GetMapping("/all") // 게시글 전체조회
-    public ResponseEntity<List<NoticeResponseDTO>> getAllNotice(){
+//    @GetMapping("/all") // 게시글 전체조회
+//    public ResponseEntity<List<NoticeResponseDTO>> getAllNotice(){
+//
+//        List<NoticeResponseDTO> noticeResponseDTOList = noticeService.findAll();
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(noticeResponseDTOList);
+//    }
 
-        List<NoticeResponseDTO> noticeResponseDTOList = noticeService.findAll();
+    @GetMapping("/all") // 게시글 전체조회 ( paging 기능 구현 )
+    public ResponseEntity<Page<NoticeResponseDTO>> getAllNotice(
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "10") int size,
+            @RequestParam(required = false, defaultValue = "id,desc") String sort){
+
+        Page<NoticeResponseDTO> noticeResponseDTOList = noticeService.findAll(page, size, sort);
 
         return ResponseEntity.status(HttpStatus.OK).body(noticeResponseDTOList);
     }
 
     @GetMapping("/detail/{NoticeId}") // 게시글 상세조회 ( Path Variable )
-    public ResponseEntity<NoticeDetailResponseDTO> getDetailNotice(@PathVariable Long NoticeId){
+    public ResponseEntity<NoticeDetailResponseDTO> getDetailNotice(@PathVariable Long NoticeId, @PageableDefault(page=1) Pageable pageable ){
 
         NoticeDetailResponseDTO noticeDetailResponseDTO = noticeService.findById(NoticeId);
 
         return ResponseEntity.status(HttpStatus.OK).body(noticeDetailResponseDTO);
     }
 
-    @GetMapping("/category") // 카테고리 별 게시글 조회 ( Query String : Requestparam )
-    public ResponseEntity<List<NoticeCategoryResponseDTO>> getCategoryNotice(@RequestParam("noticeCategory") String noticeCategory){
+//    @GetMapping("/category") // 카테고리 별 게시글 조회 ( Query String : Requestparam )
+//    public ResponseEntity<List<NoticeCategoryResponseDTO>> getCategoryNotice(@RequestParam("noticeCategory") String noticeCategory){
+//
+//        List<NoticeCategoryResponseDTO> noticeCategoryResponseDTOList = noticeService.findAllByCategory(noticeCategory);
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(noticeCategoryResponseDTOList);
+//        // return null;
+//    }
 
-        List<NoticeCategoryResponseDTO> noticeCategoryResponseDTOList = noticeService.findAllByCategory(noticeCategory);
+    @GetMapping("/category") // 카테고리 별 게시글 조회, 페이징 처리 ( Query String : Requestparam )
+    public ResponseEntity<Page<NoticeCategoryResponseDTO>> getCategoryNotice(
+            @RequestParam("noticeCategory") String noticeCategory,
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "10") int size,
+            @RequestParam(required = false, defaultValue = "id,desc") String sort ){
+
+        Page<NoticeCategoryResponseDTO> noticeCategoryResponseDTOList = noticeService.findAllByCategory(noticeCategory, page, size, sort);
 
         return ResponseEntity.status(HttpStatus.OK).body(noticeCategoryResponseDTOList);
         // return null;
     }
+
 
     @PostMapping("/addNotice") // 게시글 데이터 요청 저장
     public ResponseEntity<String> createNotice(@RequestBody List<NoticeRequestDTO> noticeRequestDTOList){
--- src/main/java/org/ayu/doyouknowback/notice/service/NoticeService.java
+++ src/main/java/org/ayu/doyouknowback/notice/service/NoticeService.java
@@ -7,6 +7,7 @@ import org.ayu.doyouknowback.notice.form.NoticeDetailResponseDTO;
 import org.ayu.doyouknowback.notice.form.NoticeRequestDTO;
 import org.ayu.doyouknowback.notice.form.NoticeResponseDTO;
 import org.ayu.doyouknowback.notice.repository.NoticeRepository;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,18 +33,40 @@ public class NoticeService {
         noticeRepository.saveAll(noticeList);
     }
 
-    @Transactional(readOnly = true)
-    public List<NoticeResponseDTO> findAll(){
+//    @Transactional(readOnly = true)
+//    public List<NoticeResponseDTO> findAll(){
+//
+//        List<Notice> noticeEntityList = noticeRepository.findAll();
+//
+//        List<NoticeResponseDTO> noticeResponseDTOList = new ArrayList<>();
+//
+//        for (Notice notice : noticeEntityList) {
+//            noticeResponseDTOList.add(NoticeResponseDTO.toDTO(notice));
+//        }
+//
+//        return noticeResponseDTOList;
+//    }
 
-        List<Notice> noticeEntityList = noticeRepository.findAll();
+    @Transactional
+    public Page<NoticeResponseDTO> findAll(int page, int size, String sort){ // 게시글 전체조회 로직 ( paging 기능 추가 )
+
+        String[] sortParams = sort.split(","); // 매개변수 sort 배열으로 나누기
+        // sortParams[1]=id, sortParams[2]=desc(내림차순: 11, 10, 9, 8, 7 .... [게시글 id 를 기준으로 한다.] )
+        // 내림차순 정렬으로 해야 첫번째 페이지에 가장 최신의 게시글이 뜨게 된다.
+
+        Sort sorting = Sort.by(Sort.Direction.fromString(sortParams[1]), sortParams[0]);
+
+        Pageable pageable = PageRequest.of(page, size, sorting);
+
+        Page<Notice> noticeEntityList = noticeRepository.findAll(pageable);
 
         List<NoticeResponseDTO> noticeResponseDTOList = new ArrayList<>();
-
         for (Notice notice : noticeEntityList) {
             noticeResponseDTOList.add(NoticeResponseDTO.toDTO(notice));
         }
 
-        return noticeResponseDTOList;
+        return new PageImpl<>(noticeResponseDTOList, pageable, noticeEntityList.getTotalElements());
+        // new PageImpl<>(참조List, Pageable, page겍체.getTotalElements()); ==> Page 객체
     }
 
     public NoticeDetailResponseDTO findById(Long id){
@@ -60,9 +83,31 @@ public class NoticeService {
         }
     }
 
-    public List<NoticeCategoryResponseDTO> findAllByCategory(String category){
+//    public List<NoticeCategoryResponseDTO> findAllByCategory(String category){
+//
+//        List<Notice> noticeList = noticeRepository.findAllByCategory(category);
+//
+//        List<NoticeCategoryResponseDTO> noticeCategoryResponseDTOList = new ArrayList<>();
+//
+//        for (Notice notice : noticeList) {
+//            noticeCategoryResponseDTOList.add(NoticeCategoryResponseDTO.toDTO(notice));
+//        }
+//
+//        return noticeCategoryResponseDTOList;
+//
+//    }
 
-        List<Notice> noticeList = noticeRepository.findAllByCategory(category);
+    public Page<NoticeCategoryResponseDTO> findAllByCategory(String category, int page, int size, String sort){
+
+        String[] sortParams = sort.split(","); // 매개변수 sort 배열으로 나누기
+        // sortParams[1]=id, sortParams[2]=desc(내림차순: 11, 10, 9, 8, 7 .... [게시글 id 를 기준으로 한다.] )
+        // 내림차순 정렬으로 해야 첫번째 페이지에 가장 최신의 게시글이 뜨게 된다.
+
+        Sort sorting = Sort.by(Sort.Direction.fromString(sortParams[1]), sortParams[0]);
+
+        Pageable pageable = PageRequest.of(page, size, sorting);
+
+        Page<Notice> noticeList = noticeRepository.findAllByCategory(category, pageable);
 
         List<NoticeCategoryResponseDTO> noticeCategoryResponseDTOList = new ArrayList<>();
 
@@ -70,7 +115,7 @@ public class NoticeService {
             noticeCategoryResponseDTOList.add(NoticeCategoryResponseDTO.toDTO(notice));
         }
 
-        return noticeCategoryResponseDTOList;
+        return new PageImpl<>(noticeCategoryResponseDTOList, pageable, noticeList.getTotalElements());
 
     }
 
