7bbc24b8757e95501d64a61303f464bb8fa7be1b
dongyeop00 <ehd_duq94@naver.com>
2025-05-31 00:14:47 +0900

    Feat : 분실습득 스케줄러 주석처리

--- src/main/java/org/ayu/doyouknowback/lost/scheduler/LostSchedulerConfiguration.java
+++ src/main/java/org/ayu/doyouknowback/lost/scheduler/LostSchedulerConfiguration.java
@@ -1,35 +1,35 @@
-package org.ayu.doyouknowback.lost.scheduler;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-
-@Component
-@RequiredArgsConstructor
-public class LostSchedulerConfiguration {
-
-    // 매일 23시~01시 사이 매 5분마다 실행 (cron 설명: 초 분 시 일 월 요일)
-    @Scheduled(cron = "0 * 23-23,0-1 * * *")
-    public void runLostPythonScript() {
-        // 로컬 환경 개발용 주소
-        String pythonScriptPath = "C:\\Doyouknow\\Doyouknow_Crawling\\lost.py";
-        // 서버 환경용 주소
-        // String pythonScriptPath = "/home/ubuntu/크롤링폴더";
-        ProcessBuilder processBuilder = new ProcessBuilder("python", pythonScriptPath);
-
-        try {
-            Process process = processBuilder.start();
-            int exitCode = process.waitFor();
-
-            if (exitCode == 0) {
-                System.out.println("lost.py 실행 성공");
-            } else {
-                System.err.println("lost.py 실행 실패, 종료 코드: " + exitCode);
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-}
+//package org.ayu.doyouknowback.lost.scheduler;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.IOException;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class LostSchedulerConfiguration {
+//
+//    // 매일 23시~01시 사이 매 5분마다 실행 (cron 설명: 초 분 시 일 월 요일)
+//    @Scheduled(cron = "0 * 23-23,0-1 * * *")
+//    public void runLostPythonScript() {
+//        // 로컬 환경 개발용 주소
+//        String pythonScriptPath = "C:\\Doyouknow\\Doyouknow_Crawling\\lost.py";
+//        // 서버 환경용 주소
+//        // String pythonScriptPath = "/home/ubuntu/크롤링폴더";
+//        ProcessBuilder processBuilder = new ProcessBuilder("python", pythonScriptPath);
+//
+//        try {
+//            Process process = processBuilder.start();
+//            int exitCode = process.waitFor();
+//
+//            if (exitCode == 0) {
+//                System.out.println("lost.py 실행 성공");
+//            } else {
+//                System.err.println("lost.py 실행 실패, 종료 코드: " + exitCode);
+//            }
+//        } catch (IOException | InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
