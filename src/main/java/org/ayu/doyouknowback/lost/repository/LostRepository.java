2b49f1a0014fd81c73c3afda79c89caae8794457
dongyeop00 <ehd_duq94@naver.com>
2025-07-09 21:43:50 +0900

    Feat : 크롤링 결과와 현재 db랑 5개 비교 후 푸시알림 전송 및 새로운 데이터 저장

--- src/main/java/org/ayu/doyouknowback/lost/repository/LostRepository.java
+++ src/main/java/org/ayu/doyouknowback/lost/repository/LostRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LostRepository extends JpaRepository<Lost, Long> {
     Page<Lost> findAll(Pageable pageable);
+    List<Lost> findTop5ByOrderByIdDesc();
 }
