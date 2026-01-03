6a03c5d1e2bdda77fbf384c57502fd82395c76dc
dongyeop00 <ehd_duq94@naver.com>
2026-01-03 19:11:12 +0900

    Refactor : RestTemplate, ObjectMapper 빈 등록

--- src/main/java/org/ayu/doyouknowback/global/config/AsyncConfig.java
+++ src/main/java/org/ayu/doyouknowback/global/config/AsyncConfig.java
@@ -1,10 +1,12 @@
 package org.ayu.doyouknowback.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.Executor;
 
@@ -27,5 +29,15 @@ public class AsyncConfig {
         log.info("PushNotificationExecutor Async Executor 초기화 완료");
 
         return executor;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 }
