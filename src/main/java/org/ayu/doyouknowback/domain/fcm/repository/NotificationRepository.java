f5d06a644d316b48ae14a999c8e8bc0a9cefe4c4
dongyeop00 <ehd_duq94@naver.com>
2026-01-05 18:35:04 +0900

    Feat : 필요한 쿼리문만 남김

--- src/main/java/org/ayu/doyouknowback/domain/fcm/repository/NotificationRepository.java
+++ src/main/java/org/ayu/doyouknowback/domain/fcm/repository/NotificationRepository.java
@@ -2,21 +2,14 @@ package org.ayu.doyouknowback.domain.fcm.repository;
 
 import org.ayu.doyouknowback.domain.fcm.domain.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-    @Query("SELECT f FROM Notification f WHERE f.token.value = :token")
+    // 토큰 조회
+    @Query("SELECT n FROM Notification n WHERE n.token.value = :token")
     Optional<Notification> findByToken(@Param("token") String token);
-
-    List<Notification> findAll();
-
-    @Modifying
-    @Query("DELETE FROM Notification f WHERE f.token.value = :token")
-    void deleteByToken(@Param("token") String token);
 }
